d01557a13f945f853ef53169a54266b3edcef773
MartinDeFrancesco <defrancescoma@gmail.com>
2022-03-18 07:02:59 -0700

    fix main.h and task 0

--- 0-display.c
+++ 0-display.c
@@ -2,5 +2,20 @@
 
 void display_complex_number(complex c)
 {
-	printf("\n%d + %di", c.real, c.imaginary);
+	if (c.im == 0)
+		printf("%.0f\n", c.re);
+	else if (c.im > 0)
+	{
+		if (c.im == 1)
+			printf("%.0f + i\n", c.re);
+		else
+			printf("%.0f + %.0fi\n", c.re, c.im);
+	}
+	else if (c.im < 0)
+	{
+		if (c.im == -1)
+			printf("%.0f - i\n", c.re);
+		else
+			printf("%.0f - %.0fi\n", c.re, (c.im * -1));
+	}
 }
--- main.h
+++ main.h
@@ -13,11 +13,11 @@
  * Description: above
  */
 
-struct complex{
-  float real;
-  float imaginary;
-}
-typedef struct complex complex_t;
+typedef struct complex
+{
+    double re;
+    double im;
+} complex;
 
 void display_complex_number(complex c);
 complex conjugate(complex c);
@@ -28,3 +28,5 @@ void substraction(complex c1, complex c2, complex *c3);
 void multiplication(complex c1, complex c2, complex *c3);
 void division(complex c1, complex c2, complex *c3);
 void complex_from_mod_arg (double m, double arg, complex *c3);
+
+#endif
